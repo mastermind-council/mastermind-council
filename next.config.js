66f86b8a415e05bb1a0ced5248e92606ad200a6e
mastermind-council <kwameanku@gmail.com>
2025-09-15 14:58:34 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; media-src 'self' blob:;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; media-src 'self' blob: data:;",
           },
         ],
       },
