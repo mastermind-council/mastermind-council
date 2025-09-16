77df31b60140757eb4ee4f07532e9cdcf4554909
mastermind-council <kwameanku@gmail.com>
2025-09-15 22:24:12 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
+    const csp = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: https:;
+      font-src 'self' data:;
+      media-src 'self' blob: data:;
+      connect-src 'self' https://api.openai.com;
+      frame-src 'none';
+    `.replace(/\n/g, '').trim();
+
     return [
       {
-        source: '/:path*',
+        source: '/(.*)', // works just like '/:path*'
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; media-src 'self' blob: data:;",
+            value: csp,
           },
         ],
       },
