da9c8be4427c96136a1350506856645630a5d0ca
mastermind-council <kwameanku@gmail.com>
2025-09-17 22:35:51 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data: blob:",
-              "media-src 'self' blob:",
-              "connect-src 'self' https:",
-              "font-src 'self' data:",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
